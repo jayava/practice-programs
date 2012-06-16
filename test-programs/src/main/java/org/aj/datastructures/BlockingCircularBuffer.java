a6468e8afe866f0d66c47b19c7b17e5037cf8448
JackBauer007 <anishjayavant@me.com>
2012-06-16 12:33:38 -0400

    some fixes made bcb paramterized

--- test-programs/src/main/java/org/aj/datastructures/BlockingCircularBuffer.java
+++ test-programs/src/main/java/org/aj/datastructures/BlockingCircularBuffer.java
@@ -4,17 +4,18 @@ import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
 
-public class BlockingCircularBuffer {
+public class BlockingCircularBuffer<E> {
 
 	private static final int MAX_SIZE = 10;
-	private final Object[] buffer;
+	private final E[] buffer;
 	private int readPos = 0, writePos = 0;
 	private final Lock bufferSyncLock = new ReentrantLock();
 	private final Condition full = bufferSyncLock.newCondition();
 	private final Condition empty = bufferSyncLock.newCondition();
 
+	@SuppressWarnings("unchecked")
 	public BlockingCircularBuffer() {
-		buffer = new Object[MAX_SIZE];
+		buffer = (E[]) new Object[MAX_SIZE];
 	}
 
 	public boolean isEmpty() {
@@ -43,73 +44,65 @@ public class BlockingCircularBuffer {
 		return bRet;
 	}
 
-	public void write(Object o) {
+	public void write(E o) throws InterruptedException {
 		try {
 			bufferSyncLock.lock();
-//			System.out.println("Thread :" + Thread.currentThread().getName()
-//					+ " is writing object :" + o + " the buffer at position :"
-//					+ writePos);
-			// invoke await in a while loop - if not enough as 
+			// System.out.println("Thread :" + Thread.currentThread().getName()
+			// + " is writing object :" + o + " the buffer at position :"
+			// + writePos);
+			// invoke await in a while loop - if not enough as
 			// it might lead to inconsistent states
 			while (isBufferFull()) {
-//				System.out.println("Buffer is full.. will need to wait");
+				// System.out.println("Buffer is full.. will need to wait");
 				full.await();
 			}
-//			System.out.println("Thread " + Thread.currentThread().getName() + 
-//					" acquired lock on a buffer that is not full");
-//			// lock acquired and circbuffer is not full
-			//Step 1: Write the object into the buffer
+			// System.out.println("Thread " + Thread.currentThread().getName() +
+			// " acquired lock on a buffer that is not full");
+			// // lock acquired and circbuffer is not full
+			// Step 1: Write the object into the buffer
 			buffer[writePos] = o;
-			//Step 2: Calculate the new value of writePos
-			if(writePos == MAX_SIZE -1)
+			// Step 2: Calculate the new value of writePos
+			if (writePos == MAX_SIZE - 1)
 				writePos = 0; // reset to zero as this is a circular buffer
 			else
 				++writePos;
-			// Write complete - now signal the threads waiting on an empty circular buffer.
+			// Write complete - now signal the threads waiting on an empty
+			// circular buffer.
 			empty.signal();
-			
-		} catch (InterruptedException e) {
-			System.out.println("Thread :" + Thread.currentThread().getName()
-					+ " was interrupted");
-			e.printStackTrace();
 		} finally {
 			bufferSyncLock.unlock();
 		}
 	}
-	
-	public Object read(){
-		Object retObj = null;
+
+	public E read() throws InterruptedException {
+		E retObj = null;
 		try {
-			//Lock the buffer
+			// Lock the buffer
 			bufferSyncLock.lock();
-//			System.out.println("Thread :" + Thread.currentThread().getName()
-//					+ " is reading object from the buffer at position :"
-//					+ readPos);
-			while(isBufferEmpty()){
+			// System.out.println("Thread :" + Thread.currentThread().getName()
+			// + " is reading object from the buffer at position :"
+			// + readPos);
+			while (isBufferEmpty()) {
 				System.out.println("Buffer empty - must wait");
 				empty.await();
 			}
 			// acquired lock on a nonempty buffer
-//			System.out.println("Thread " + Thread.currentThread().getName() + 
-//					" acquired lock on a buffer that is not empty");
-//			//Step 1: read the object from the buffer
+			// System.out.println("Thread " + Thread.currentThread().getName() +
+			// " acquired lock on a buffer that is not empty");
+			// //Step 1: read the object from the buffer
 			retObj = buffer[readPos];
 			// Step 2: calculate readPos
-			if(readPos == MAX_SIZE -1)
+			if (readPos == MAX_SIZE - 1)
 				readPos = 0; // reset to zero as this is a circular buffer
 			else
 				++readPos;
-			// read complete - now signal the threads waiting on a full circular buffer.
+			// read complete - now signal the threads waiting on a full circular
+			// buffer.
 			full.signal();
-			
-		} catch (InterruptedException e) {
-			System.out.println("Thread :" + Thread.currentThread().getName()
-					+ " was interrupted");
-			e.printStackTrace();
 		} finally {
 			bufferSyncLock.unlock();
 		}
-		
+
 		return retObj;
 	}
 
--- test-programs/src/main/java/org/aj/testprograms/MergeSort.java
+++ test-programs/src/main/java/org/aj/testprograms/MergeSort.java
@@ -24,13 +24,43 @@ public class MergeSort {
 			int middle = (low + high) / 2;
 			mergesort(low, middle, numbers);
 			mergesort(middle + 1, high, numbers);
-			merge(low, high,numbers);
+			merge(low, high, numbers);
 		}
 	}
 
 	private static void merge(int low, int high, int[] numbers) {
-		// TODO Auto-generated method stub
 
+		// copy into helper
+		int[] helper = new int[high - low + 1];
+		int k = low;
+		for (int i = 0; i < helper.length; i++) {
+			helper[i] = numbers[k++];
+		}
+		
+		int middle = (high - low)/2;
+		int j = middle + 1;
+		int i = 0;
+		k = low;
+
+		while (i <= middle) {
+			if (j <= high) {
+				if (helper[i] <= helper[j]) {
+					numbers[k] = helper[i];
+					i++;
+				} else {
+					numbers[k] = helper[j];
+					j++;
+				}
+			} else {
+				numbers[k] = helper[i];
+				i++;
+			}
+			k++;
+		}
+
+		while (j <= high) {
+			numbers[k++] = helper[j++];
+		}
 	}
 
 }
