7b4fe4351274d4958e0bd4d4f8ab7f5050c3a951
JackBauer007 <anishjayavant@me.com>
2012-06-18 21:39:06 -0400

    integer ops

---
+++ test-programs/src/main/java/org/aj/testprograms/IntegerOperationsUtils.java
@@ -0,0 +1,89 @@
+package org.aj.testprograms;
+
+public class IntegerOperationsUtils {
+
+	public static int negate(int a) {
+		return add(~a, 1);
+	}
+
+	public static int add(int a, int b) {
+		//
+		int sum = a ^ b;
+		int carry = a & b;
+
+		while (carry != 0) {
+			carry <<= 1;
+			a = sum;
+			b = carry;
+			sum = a ^ b;
+			carry = a & b;
+		}
+
+		return sum;
+	}
+
+	public static int subtract(int a, int b) {
+		return add(a, negate(b));
+	}
+
+	public static int multiplyUsingBitwise(int a, int b) {
+		if (a == 0 || b == 0)
+			return 0;
+
+		int signFlag = 0;
+
+		if (a < 0) {
+			a = negate(a);
+			signFlag ^= 1;
+		}
+		if (b < 0) {
+			b = negate(b);
+			signFlag ^= 1;
+		}
+
+		int result = 0;
+
+		while (b != 0) {
+			if ((b & 1) == 1) {
+				result += a;
+			}
+
+			a <<= 1;
+			b >>= 1;
+		}
+
+		if ((signFlag & 1) == 1)
+			return negate(result);
+		else
+			return result;
+	}
+
+	public static int multiplyUsingAdd(int a, int b) {
+		// multiply using add function
+		// check for zeroes
+		if (a == 0 || b == 0)
+			return 0;
+
+		int signFlag = 0;
+
+		if (a < 0) {
+			a = negate(a);
+			signFlag ^= 1;
+		}
+		if (b < 0) {
+			b = negate(b);
+			signFlag ^= 1;
+		}
+
+		int result = 0;
+		int count = 1;
+		while (count <= b) {
+			result = add(result, a);
+			count = add(count, 1);
+		}
+		if ((signFlag & 1) == 1)
+			return negate(result);
+		else
+			return result;
+	}
+}
---
+++ test-programs/src/test/java/org/aj/testprograms/TestIntegerOperationsUtils.java
@@ -0,0 +1,64 @@
+package org.aj.testprograms;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class TestIntegerOperationsUtils {
+
+	@Test
+	public void testNegation() {
+		assertEquals(IntegerOperationsUtils.negate(4), -4);
+		assertEquals(IntegerOperationsUtils.negate(-4), 4);
+		assertEquals(IntegerOperationsUtils.negate(0), 0);
+		assertEquals(IntegerOperationsUtils.negate(-0), 0);
+	}
+	
+	@Test
+	public void testAddition() {
+		assertEquals(IntegerOperationsUtils.add(3, 4),7);
+		assertEquals(IntegerOperationsUtils.add(4, 5),9);
+		assertEquals(IntegerOperationsUtils.add(-4, 5),1);
+		assertEquals(IntegerOperationsUtils.add(-4, -5),-9);
+		assertEquals(IntegerOperationsUtils.add(-5, 4),-1);
+		assertEquals(IntegerOperationsUtils.add(0, 0),0);
+		assertEquals(IntegerOperationsUtils.add(0, 3),3);
+	}
+
+	@Test
+	public void testSubtraction() {
+		assertEquals(IntegerOperationsUtils.subtract(5, 4), 1);
+		assertEquals(IntegerOperationsUtils.subtract(4, 5), -1);
+		assertEquals(IntegerOperationsUtils.subtract(4, 0), 4);
+		assertEquals(IntegerOperationsUtils.subtract(0, 4), -4);
+		assertEquals(IntegerOperationsUtils.subtract(0, 0), 0);
+		assertEquals(IntegerOperationsUtils.subtract(0, -0), 0);
+		assertEquals(IntegerOperationsUtils.subtract(-4, 0), -4);
+		assertEquals(IntegerOperationsUtils.subtract(-4, 4), -8);
+		assertEquals(IntegerOperationsUtils.subtract(-4, 5), -9);
+		assertEquals(IntegerOperationsUtils.subtract(-4, -4), 0);
+		assertEquals(IntegerOperationsUtils.subtract(6, -4), 10);
+	}
+	
+	@Test
+	public void testMultiplyUsingAdd() {
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(3, 4),12);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(-4, -5),20);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(-4, 4),-16);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(-4, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(4, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(0, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingAdd(4, -7),-28);
+	}
+	
+	@Test
+	public void testMultiplyUsingBitwise() {
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(3, 4),12);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(-4, -5),20);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(-4, 4),-16);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(-4, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(4, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(0, 0),0);
+		assertEquals(IntegerOperationsUtils.multiplyUsingBitwise(4, -7),-28);
+	}
+}
